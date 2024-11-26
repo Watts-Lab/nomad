2bbd0399ff68d825819c69eefe3b8e51e501904d
Thomas L <130534235+thom-li@users.noreply.github.com>
2024-11-26 10:12:20 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='nomad',
+    version='0.0.1',
+    author='Placeholder',
+    author_email='your.email@example.com',
+    description='Placeholder',
+    url='https://github.com/Watts-Lab/nomad',
+    install_requires=[
+        'pandas',
+        'numpy',
+        'datetime',
+        'pyspark',
+        'shapely',
+        'matplotlib',
+        'networkx',
+        'nx_parallel'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
