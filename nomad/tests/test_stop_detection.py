9e66602fbce406a5e6d07ceaef62687af902a984
Thomas L <130534235+thom-li@users.noreply.github.com>
2025-04-14 20:55:52 -0400

    Update Lachesis code for research
    update the stop detection files
    
    ---------
    
    Co-authored-by: Andrés Mondragón <101443712+andresmondragont@users.noreply.github.com>
    Co-authored-by: Jorge Francisco Barreras <franciscojosebarreras@gmail.com>

--- nomad/tests/test_stop_detection.py
+++ nomad/tests/test_stop_detection.py
@@ -3,6 +3,7 @@ import geopandas as gpd
 from pandas.testing import assert_frame_equal
 from scipy.spatial.distance import pdist, cdist
 import pygeohash as gh
+from datetime import datetime, timedelta
 import itertools
 from collections import defaultdict
 import pytest
@@ -12,9 +13,14 @@ from nomad import constants
 from nomad import filters
 import nomad.stop_detection.ta_dbscan as DBSCAN
 import nomad.stop_detection.lachesis as LACHESIS
-
 import pdb
 
+@pytest.fixture
+def agent_traj_ground_truth():
+    test_dir = Path(__file__).resolve().parent
+    traj_path = test_dir.parent / "data" / "gc_3_stops.csv"
+    df = loader.from_file(traj_path, timestamp='unix_timestamp', datetime='local_timestamp', user_id='identifier')
+    return df
 
 @pytest.fixture
 def base_df():
@@ -189,6 +195,52 @@ def test_lachesis_name_handling_from_variants(single_user_df, column_variations,
 ##########################################
 ####          LACHESIS TESTS          #### 
 ##########################################
+# Test to see if number of unique labels in lachesis_labels matches with number of stops from stop table
+def test_lachesis_number_labels(single_user_df):
+    """Tests if the output of Lachesis labels has same number of unique labels as the Lachesis stop table."""
+    traj_cols = {
+        "timestamp": "timestamp",
+        "x": "x", "y": "y"
+    }
+    
+    single_user_df = loader.from_df(single_user_df, traj_cols=traj_cols, parse_dates=True, mixed_timezone_behavior="utc")
+
+    stops_df = LACHESIS.lachesis(
+        traj=single_user_df,
+        dur_min=5,
+        dt_max=10,
+        delta_roam=100,
+        traj_cols=traj_cols,
+        complete_output=False,
+        keep_col_names=False
+    )
+    
+    labels_df = LACHESIS._lachesis_labels(
+        traj=single_user_df,
+        dur_min=5,
+        dt_max=10,
+        delta_roam=100,
+        traj_cols=traj_cols,
+        keep_col_names=False
+    )
+
+    labels_df = labels_df[labels_df['cluster'] != -1]
+
+    assert len(stops_df) == labels_df['cluster'].nunique()
+
+# Test to see if they identify correct number of stops
+def test_lachesis_ground_truth(agent_traj_ground_truth):
+    lachesis_params = (45, 60, 3)
+    traj_cols = {'user_id':'identifier',
+             'x':'x',
+             'y':'y',
+             'timestamp':'unix_timestamp'}
+    lachesis_out = LACHESIS._lachesis_labels(agent_traj_ground_truth,
+                                             *lachesis_params,
+                                             traj_cols,
+                                             keep_col_names=False)
+    num_clusters = sum(lachesis_out.cluster.unique() > -1)
+    assert num_clusters == 3
 
 ##########################################
 ####           DBSCAN TESTS           #### 
