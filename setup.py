5e9c7d4f34c31f80c3da28996e8cce20375b24bd
Thomas L <130534235+thom-li@users.noreply.github.com>
2025-04-16 13:36:18 -0400

    Filtering demo (#63)
    - Update the filter functions to improve user input and usage of
    functions
    - Improve test suite to accommodate changes to functions
    - All tests pass
    - Add garden city to demo notebook visualization
    
    ---------
    
    Co-authored-by: Thomas Li <thomli@sas.upenn.edu>

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setup(
     url='https://github.com/Watts-Lab/nomad',
     version='0.0.1',
     author='Francisco Barreras, Thomas Li, Federico Delussu, Andres Mondragon',
-    author_email='fbarrer@sas.upenn.edu, thomli@sas.upenn.edu, fedde@dtu.dk',
+    author_email='fbarrer@sas.upenn.edu, thomli@sas.upenn.edu, fedde@dtu.dk, amt00@sas.upenn.edu',
     description='NOMAD provides a repository of processing and analysis methods for GPS mobility data, centralizing tools necessary for large-scale analyses. This repository will serve the dual function of facilitating research for first-time users and enhancing the replicability and robustness of existing methodologies. By collecting many such methods in a single place and documenting their assumptions and robustness metrics, NOMAD aims to enhance methodological transparency and replicability of research.',
     packages=find_packages(include=['nomad', 'nomad.*']),
     install_requires=[
         'pandas',
+        'geopandas',
         'numpy',
         'datetime',
         'pyspark',
@@ -33,5 +34,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.10',
 )
