059c093bc1f720a5a23750e1fb75b148b9558e9e
paco-barreras <franciscojosebarreras@gmail.com>
2026-02-12 16:05:59 -0500

    migrated to density_based.py only and debugged a bit

--- nomad/stop_detection/density_based.py
+++ nomad/stop_detection/density_based.py
@@ -2,160 +2,160 @@ import numpy as np
 import pandas as pd
 from sklearn.cluster import DBSCAN
 import pdb
+import geopandas as gpd
 
-##todo: impl secscan by following dbscan.py
+
 def seqscan_labels(
-    df,
-    eps,
-    delta, # presence treshold: min time to count as stay
+    data,
+    dist_thresh,
+    min_dur=5,
+    time_thresh=90,
     min_pts=3,
     user_id=None,
-    x_col="x",
-    y_col="y",
-    t_col="timestamp",
+    traj_cols=None,
+    **kwargs
 ):
-    data = df.copy()
+    if not isinstance(data, (pd.DataFrame, gpd.GeoDataFrame)):
+         raise TypeError("Input 'data' must be a pandas DataFrame or GeoDataFrame.")
+    if data.empty:
+        return pd.DataFrame()
 
     if user_id is not None:
         data = data.loc[data["user_id"] == user_id].copy()
 
-    # not necessary?
-    # sort points and keep orig idex so can return aligned labels
-    # data = data.sort_values(t_col, kind="mergesort").reset_index(drop=False)
-    orig_idx = data["index"].to_numpy()
+    t_key, coord_key1, coord_key2, use_datetime, use_lon_lat = utils._fallback_st_cols(
+        data.columns, traj_cols, kwargs
+    )        
+    traj_cols = loader._parse_traj_cols(data.columns, traj_cols, kwargs)
 
-    X = data[[x_col, y_col]].to_numpy()
-    t = data[t_col].to_numpy().astype(float)
-    n = len(data)
-    out = np.full(n, -1, dtype=int)
+    # Tests to check for spatial and temporal columns
+    loader._has_spatial_cols(data.columns, traj_cols)
+    loader._has_time_cols(data.columns, traj_cols)
 
-    # presence def: stay must satisfy a minimum time spent in the region
-    def presence(curr_window, lo, hi):
-        total = 0.0
-        for k in range(lo, hi):
-            if curr_window[k - lo] and curr_window[k + 1 - lo]:
-                total += (t[k + 1] - t[k])
-        return total
+    valid_times = to_timestamp(data[traj_cols[t_key]]) if use_datetime else data[traj_cols[t_key]]
+    neighbor_dict = _find_neighbors(data, time_thresh, dist_thresh, use_lon_lat, use_datetime, traj_cols)
     
-    def expand(active_cluster, lo, hi, newPoint):
-        labels = DBSCAN(eps=eps, min_samples=min_pts).fit_predict(X[lo:hi+1])
-        return labels, (labels[newPoint - lo] == active_cluster)
+    cluster_df = pd.Series(-1, index=valid_times, name='cluster')
+    core_df = pd.Series(-1, index=valid_times, name='core')
 
     # SeqScan main loop start
-    start = 0        # current time context ind
-    end = -1         # last ind in active stay
-    active_cid = None  # which DBSCAN cluster id is curr stay
-    labels = 0      # permanent stay label
+    start = valid_times.iloc[0]        # current time context start
+    end = valid_times.iloc[0] - 1        # current candidate to cut time context
+    #find cluster routine start
+    temp_neighbor_dict = {} # in time context
+    active_cid = -1  # or -1, which DBSCAN cluster id is curr stay
+    # thus active_cid - 1 is the preceeding cluster id
+    #TODO: global?
+    temp_cid = 100000 # for internal dbscan until new active cluster is found
 
-    for c in range(n):
-        lo, hi = start, c # curr timeContext
+    def findCluster(start_time, t):
+        nonlocal temp_neighbor_dict, temp_cid, active_cid, start, end
+        # want time indices to be >= start
+        temp_neighbor_dict = {idx:nbs for idx, nbs in temp_neighbor_dict.items() if idx >= start_time}
+        temp_neighbor_dict[t] = {nb for nb in neighbor_dict[t] if t > nb >= start_time}
+        t_is_core = len(temp_neighbor_dict[t]) >= min_pts
+        if t_is_core:
+            temp_cid += 1
+            core_df[t] = temp_cid
+            cluster_df[t] = temp_cid
 
-        # if already have active stay,, expand
-        if active_cid is not None:
-            _, ok = expand(active_cid, lo, hi, c)
-            if ok:
-                end = c
-                continue
+        for s in temp_neighbor_dict[t]:
+            temp_neighbor_dict[s].add(t)
+            if len(temp_neighbor_dict[s]) >= min_pts:
+                if core_df[s] >= 0:
+                    # already was a temporary core point
+                    # just merge "through" t
+                    # if t is core, relabel s and its neighbors (relabeling for merge)
+                    if t_is_core:
+                        cluster_df.loc[core_df==core_df[t]] = core_df[s]
+                        core_df.loc[core_df==core_df[t]] = core_df[s]
+                    
+                elif cluster_df[s] >= 0:
+                    core_df[s] = cluster_df[s]
+                    if t_is_core:
+                        cluster_df.loc[core_df==core_df[t]] = core_df[s]
+                        core_df.loc[core_df==core_df[t]] = core_df[s]
+                    for nb in temp_neighbor_dict[s]:
+                        if core_df[nb]>=0:
+                            # TODO: border points might be neighbors of 2 disconnected core points
+                            # every core point and every neighor of core point should be relabeled
+                            nb_labs = set()
+                            for nb in temp_neighbor_dict[s]:
+                                if core_df[nb] >= 0:
+                                    # for later relabel of entire connected component
+                                    nb_labs.add(core_df[nb])
+                                else:
+                                    cluster_df[nb] = core_df[s] # (re) assign border point                            
+                            # bulk relabel and merge of all affected connected components
+                            for lab in nb_labs:
+                                core_df.loc[core_df == lab] = core_df[s]
+                                cluster_df.loc[cluster_df == lab] = core_df[s]
+                
+                elif cluster_df[s] == -1: # is a new cluster
+                    if t_is_core:
+                        core_df[s] = core_df[t]
+                        cluster_df[s] = cluster_df[t]
+                    else:
+                        temp_cid += 1 # increase temporary label counter
+                        core_df[s] = temp_cid
+                        cluster_df[s] = temp_cid
+                        # propagate label
+                        for nb in temp_neighbor_dict[s]:
+                            # there is no case in which nb is core point: otherwise cluster_df[s] would have had a label
+                            cluster_df[nb] = core_df[s]
 
-        # if can't expand, look for new stay in segment after end of last stay
-        seg_lo, seg_hi = end + 1, c
-        next_cid = None
 
-        if seg_lo <= seg_hi:
-            # Run DBSCNA on potential segment
-            labels_seg = DBSCAN(eps=eps, min_samples=min_pts).fit_predict(X[seg_lo:seg_hi+1])
-
-            # Find first non-noise cluster >= delta
-            #sorted?
-            for cid in sorted(set(labels_seg)):
-                if cid == -1:
-                    continue
-                curr_window = (labels_seg == cid)
-                if (presence(curr_window, seg_lo, seg_hi) >= delta):
-                    next_cid = cid
-                    break
+        window_mask = (cluster_df.index >= start_time) & (cluster_df.index <= t)
+        cand = cluster_df.loc[window_mask & (cluster_df >= 0)]
         
-        # if find next stay cluster, close old stay
-        if next_cid is not None:
-            # close old stay, assign perm label
-            if active_cid is not None:
-                labels_old = DBSCAN(eps=eps, min_samples=min_pts).fit_predict(X[start:end+1])
-                for i in range(start, end + 1):
-                    if labels_old[i - start] == active_cid:
-                        out[i] = labels
-                # move stay labels
-                labels += 1
-                start = end + 1
-        # switch active stay
-            active_cid = next_cid
-            end = c
-
-    return pd.Series(out, index=orig_idx, name="seqscan")
+        if not cand.empty:
+            spans = cand.index.to_series().groupby(cand, sort=False).agg(["first", "last"])
+            eligible = spans[(spans["last"] - spans["first"]) >= (min_dur * 60)]
         
+            if not eligible.empty:
+                c = eligible.index[0]
+        
+                active_cid += 1
+                keep = window_mask & (cluster_df == c)
+                drop = window_mask & ~keep
+        
+                cluster_df.loc[drop] = -1
+                core_df.loc[drop] = -1
+                cluster_df.loc[keep] = active_cid
+                core_df.loc[keep] = active_cid
+        
+                end = spans.loc[c, "last"]
+                start = start_time
+                temp_cid = 100000
+                return True
+                # vars changed: temp_neighbors_df, core_df, cluster_df, active_cid, end, temp_cid
+                
+        # vars changed: temp_neighbors_df, core_df, cluster_df
+        return False
 
-    # while start < n:
-    #     # print("Entering while")
+    for t in valid_times:
+        if active_cid == -1:
+            findCluster(start, t)
+        else:
+            temp_neighbor_dict[t] = {nb for nb in neighbor_dict[t] if t > nb >= start}
+            t_is_core = len(temp_neighbor_dict[t]) >= min_pts
+            if t_is_core:
+                for nb in core_df[core_df == active_cid].index:
+                    if t in neighbor_dict[nb]:
+                        core_df[t] = active_cid
+                        cluster_df[t] = active_cid
+                        end = t
+                    else:
+                        continue
+            else:
+                findCluster(end + 1, t)
+                
+    cluster_df.loc[cluster_df > 100000] = -1
+    core_df.loc[core_df > 100000] = -1
+    output = pd.DataFrame({'cluster': cluster_df, 'core': core_df})
 
-    #     if end < start:
-    #         end = start
-    #     if end >= n:
-    #         break
-
-    #     # 1) DBSCAN on window W=[start,end]
-    #     Xw = X[start:end+1]
-    #     db = DBSCAN(eps=eps, min_samples=min_pts + 1)
-    #     labels = db.fit_predict(Xw)
-    #     last_db[start:end+1] = labels + current_label
-
-    #     # 2) Determine how many clusters exist (excluding noise=-1)
-    #     # clusters = sorted([c for c in set(labels) if c != -1])
-
-    #     # grow window
-    #     if not(labels > 0).any():
-    #         end += 1
-    #         continue
-
-    #     # Else: "cut and log" first cluster (prefer label 0)
-
-    #     # TODO: labels = an array of len w, core_indices also array, we want to find np.where(labels[core_indices]==0[-1]])
-    #     # want the last core indices that is value 0
-    #     in_target = (labels == 0)
-
-    #     # Core points are indices in the window that are core samples
-    #     core_rel = set(getattr(db, "core_sample_indices_", []))
-
-    #     # Latest core point inside the target cluster (by window index)
-    #     core_in_target = [i for i in np.where(in_target)[0] if i in core_rel]
-    #     # TODO: consider [-1] instead of max, for computational reasons
-    #     cut_rel = max(core_in_target)
-
-    #     cut_global = start + cut_rel
-
-    #     # TODO: start after the cut maybe?
-    #     # Permanently label all points in target cluster strictly before cut
-    #     # TODO: rewrite to be simpler, is sorted so should be easier
-    #     finalize_rel = np.where(in_target & (np.arange(len(labels)) < cut_rel))[0]
-    #     print("finalizerel", finalize_rel)
-    #     finalize_global = start + finalize_rel
-    #     print("finalize global", finalize_global)
-
-    #     # assign only those not already assigned
-    #     mask = (perm.iloc[finalize_global] == -1)
-    #     print("mask:", mask)
-    #     perm.iloc[finalize_global[mask]] = current_label
-
-    #     # move window start to cut point; reset window
-    #     start = cut_global
-    #     end = start
-    #     current_label += 1
-    # print("finished while")
-
-    # out = data.copy()
-    # print("out", out)
-    # out["sw_label"] = perm
-    # out["sw_dbscan_last"] = last_db
-    # return perm
-
-#TODO: dont use np.where, key can use same indices 1-n, look at exp_hdbscan_paper dbscan impl 
-#TODO: perm should be labels
-#TODO: PUSH TO NEW BRANCH, change back to hdbscan, git force pull, switch back to new branch (git checkout), git merge
+    if return_cores:
+        return output.set_axis(data.index)
+    else:
+        labels = output.cluster
+        return labels.set_axis(data.index)
