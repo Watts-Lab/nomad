d38b8463bedadcdc596e87c90353718ddca257c4
Carol Tu <caroltu135987@gmail.com>
2026-02-16 15:36:49 -0500

    bug fixes

--- nomad/stop_detection/density_based_v1.py
+++ nomad/stop_detection/density_based_v1.py
@@ -1,19 +1,22 @@
 import numpy as np
 import pandas as pd
-from sklearn.cluster import DBSCAN
-import pdb
 import geopandas as gpd
+import nomad.io.base as loader
+from nomad.stop_detection import utils
+from nomad.stop_detection.preprocessing import _find_neighbors
+from nomad.filters import to_timestamp
 
 
 def seqscan_labels(
     data,
     dist_thresh,
-    min_dur, # min time to count as stay in minutes
+    time_thresh,
+    min_dur,
     min_pts=3,
     user_id=None,
-    x_col="x",
-    y_col="y",
-    t_col="timestamp",
+    return_cores=False,
+    traj_cols=None,
+    **kwargs,
 ):
     if not isinstance(data, (pd.DataFrame, gpd.GeoDataFrame)):
          raise TypeError("Input 'data' must be a pandas DataFrame or GeoDataFrame.")
@@ -23,9 +26,9 @@ def seqscan_labels(
     if user_id is not None:
         data = data.loc[data["user_id"] == user_id].copy()
 
-    t_key, coord_key1, coord_key2, use_datetime, use_lon_lat = utils._fallback_st_cols(
+    t_key, use_datetime, use_lon_lat = utils._fallback_st_cols(
         data.columns, traj_cols, kwargs
-    )        
+    )
     traj_cols = loader._parse_traj_cols(data.columns, traj_cols, kwargs)
 
     # Tests to check for spatial and temporal columns
@@ -43,19 +46,18 @@ def seqscan_labels(
     start = valid_times[0]        # current time context start
     end = valid_times[0] - 1        # current candidate to cut time context
     #find cluster routine start
-    temp_neighbor_dict = {} # in time context
-    active_cid = -1  # or -1, which DBSCAN cluster id is curr stay
+    temp_neighbor_dict = {}
+    active_cid = -1
     # thus active_cid - 1 is the preceeding cluster id
-    #TODO: global?
     temp_cid = 100000 # for internal dbscan until new active cluster is found
 
     def findCluster(start_time, t):
+        nonlocal temp_neighbor_dict, temp_cid, active_cid, start, end
         # should ensure only have cluster from time context, should never be older than t
-        # want time indices to be >= start
-        temp_neighbor_dict = {idx:nbs for idx, nbs in temp_neighbor_dict.items() if idx >= start}
-        temp_neighbor_dict[t] = {nb for nb in neighbor_dict[t] if t > nb >= start}
-        t_is_core = len(temp_neighbor_dict[t]) >= min_pts
-        if t_is_core:
+        temp_neighbor_dict = {idx:nbs for idx, nbs in temp_neighbor_dict.items() if idx >= start_time}
+        temp_neighbor_dict[t] = {nb for nb in neighbor_dict[t] if t > nb >= start_time}
+        curr_is_core = len(temp_neighbor_dict[t]) >= min_pts
+        if curr_is_core:
             temp_cid += 1
             core_df[t] = temp_cid
             cluster_df[t] = temp_cid
@@ -67,17 +69,17 @@ def seqscan_labels(
                     # already was a temporary core point
                     # just merge "through" t
                     # if t is core, relabel s and its neighbors (relabeling for merge)
-                    if t_is_core:
+                    if curr_is_core:
                         cluster_df.loc[core_df==core_df[t]] = core_df[s]
                         core_df.loc[core_df==core_df[t]] = core_df[s]
                     
                 elif cluster_df[s] >= 0:
                     core_df[s] = cluster_df[s]
-                    if t_is_core:
+                    if curr_is_core:
                         cluster_df.loc[core_df==core_df[t]] = core_df[s]
                         core_df.loc[core_df==core_df[t]] = core_df[s]
                     for nb in temp_neighbor_dict[s]:
-                        if core_df[nb]:
+                        if core_df[nb] >= 0:
                             # TODO: border points might be neighbors of 2 disconnected core points
                             # every core point and every neighor of core point should be relabeled
                             nb_labs = set()
@@ -93,7 +95,7 @@ def seqscan_labels(
                                 cluster_df.loc[cluster_df == lab] = core_df[s]
                 
                 elif cluster_df[s] == -1: # is a new cluster
-                    if t_is_core:
+                    if curr_is_core:
                         core_df[s] = core_df[t]
                         cluster_df[s] = cluster_df[t]
                     else:
@@ -114,11 +116,13 @@ def seqscan_labels(
                 # get rid?
                 active_cid = active_cid + 1
                 # disregard all other temporary labels
-                cluster_df.loc[start, t].loc[lambda s: s != c] = -1
-                core_df.loc[start, t].loc[lambda s: s != c] = -1
+                # CHANGE: cluster_df.loc[start, t] is invalid for a Series
+                mask = (cluster_df.index >= start_time) & (cluster_df.index <= t)
+                cluster_df.loc[mask & (cluster_df != c)] = -1
+                core_df.loc[mask & (core_df != c)] = -1
                 # log permanent label for active cluster
-                cluster_df.loc[start, t].loc[lambda s: s == c] = active_cid
-                core_df.loc[start, t].loc[lambda s: s == c] = active_cid
+                cluster_df.loc[mask & (cluster_df == c)] = active_cid
+                core_df.loc[mask & (core_df == c)] = active_cid
                 end = cluster.index.max()
                 start = start_time
                 temp_cid = 100000
@@ -129,21 +133,32 @@ def seqscan_labels(
 
 
 
-    for t in valid_times:
+    for curr_time in valid_times:
         if active_cid == -1:
-            findCluster(start, t)
+            findCluster(start, curr_time)
         else:
-            t_nb = {nb for nb in neighbor_dict[t] if t > nb >= start}
-            t_is_core = len(t_nb) >= min_pts
-            if t_is_core:
+            curr_nb = {nb for nb in neighbor_dict[curr_time] if curr_time > nb >= start}
+            curr_is_core = len(curr_nb) >= min_pts
+            if curr_is_core:
                 for nb in core_df[core_df == active_cid].index:
-                    if t in neighbor_dict[nb]:
-                        core_df[t] = active_cid
-                        end = t
+                    if curr_time in neighbor_dict[nb]:
+                        core_df[curr_time] = active_cid
+                        cluster_df[curr_time] = active_cid
+                        end = curr_time
                     else:
                         continue
             else:
-                findCluster(end + 1, t)
+                # allow border points reachable from active core points
+                for nb in core_df[core_df == active_cid].index:
+                    if curr_time in neighbor_dict[nb]:
+                        cluster_df[curr_time] = active_cid
+                        end = curr_time
+                        break
+                else:
+                    # CHANGE: is this necessary? look for next actual timestamp, but is it sequential already
+                    next_candidates = valid_times[valid_times > end]
+                    next_start = next_candidates.iloc[0] if len(next_candidates) else end
+                    findCluster(next_start, curr_time)
     cluster_df.loc[cluster_df > 100000] = -1
     core_df.loc[core_df > 100000] = -1
     output = pd.DataFrame({'cluster': cluster_df, 'core': core_df})
